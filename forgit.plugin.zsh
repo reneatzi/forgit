ea79bb7cb2706d3b5d5dc6e8f3fb0d69738887a7
René Atzenhofer <rene.atzenhofer@gmail.com>
2020-03-08 19:25:27 +0100

    Bind alt-p and alt-n to preview-page-up and -down

--- forgit.plugin.zsh
+++ forgit.plugin.zsh
@@ -203,8 +203,8 @@ FORGIT_FZF_DEFAULT_OPTS="
 $FZF_DEFAULT_OPTS
 --ansi
 --height='80%'
---bind='alt-k:preview-up,alt-p:preview-up'
---bind='alt-j:preview-down,alt-n:preview-down'
+--bind='alt-k:preview-up,alt-p:preview-page-up'
+--bind='alt-j:preview-down,alt-n:preview-page-down'
 --bind='ctrl-r:toggle-all'
 --bind='ctrl-s:toggle-sort'
 --bind='?:toggle-preview'
